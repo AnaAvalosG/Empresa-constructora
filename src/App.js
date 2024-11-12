991d4620c3f2d68ffc10a983ff1c79ad27945a4e
AnaCodes <avalos.anaines@gmail.com>
2024-11-12 19:21:58 +0100

    paginas que contendra mi pagina web

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Inicio from './Pages/Inicio/Inicio';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       Reco | inicio | contacto | quienes somos |nuestras obras | BUSCADOR
       </header>
+      <p>Body <br></br>
+      <br></br>
+      </p>
+      
+      <footer className='App-footer'>Footer</footer>
     </div>
   );
 }
---
+++ src/Pages/Inicio/Inicio.js
@@ -0,0 +1,21 @@
+//daremos la bienvenida al usuario e incluiremos el
+//contenido que consideremos oportuno y que pueda aportar un valor añadido.
+
+
+function Inicio() {
+    return (
+      <div className="App">
+        <header className="App-header">
+         fsggdgs
+        </header>
+        <div>
+            <h1>Reco construcciones</h1> <br />
+           <h2>Always in process</h2>
+        </div>
+        <p>fssasadsa</p>
+        <footer className='App-footer'>sdggdfds cccc</footer>
+      </div>
+    );
+  }
+
+  export default Inicio;
